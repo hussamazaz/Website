d7f8bb65d78ff0ea436f3177babd137a22ef59b0
hussamazaz <mr.7usoom@hotmail.com>
2017-04-28 17:35:18 +0300

    Add version 1

---
+++ ourservices.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Smart Car WorkShop</title>
+
+<link href="CSS/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<table width="700" border="0" align="center" cellpadding="0" cellspacing="5">
+  <tr>
+    <td align="center" colspan="2"><h1><a href="index.php">SMART CAR WORKSHOP</a></h1></td>
+  </tr>
+  <tr>
+    <td align="center">&nbsp;</td>
+    <td rowspan="5" width="550"> hi every one  </td>
+  </tr>
+  <tr>
+    <td align="center"><a href="login.php" class="indexbott" >Login</a></td>
+  </tr>
+  <tr>
+    <td align="center"><a href="aboutus.php" class="indexbott">About Us</a></td>
+  </tr>
+  <tr>
+    <td align="center"><a href="ourservices.php" class="indexbott">Our Services</a></td>
+  </tr>
+  <tr>
+    <td align="center"><a href="contactus.php" class="indexbott">Contact Us</a></td>
+  </tr>
+</table>
+</body>
+</html>
